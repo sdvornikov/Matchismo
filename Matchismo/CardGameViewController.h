b0905f01ab311d100c5c5a509fdfec34ac9c3566
Sergey <sergey.dvornikov@gmail.com>
2013-03-02 12:12:43 +0400

    Made public API of a generic CardMatchingVC

--- Matchismo/CardGameViewController.h
+++ Matchismo/CardGameViewController.h
@@ -13,25 +13,25 @@
 
 // Override to return specific instance of Deck subclass
 // Default: instance of Deck
-//- (Deck*)createNewDeck;
+- (Deck*)createNewDeck;
 
 // Override to set gamemode
 // Default: TWO_CARDS_MATCHING_GAME
-//- (int)gameMode;
+- (int)gameMode;
 
 // Override to set UIButton state for card faceUp status
 // Default: switches buttons selected state
-//- (void)updateCardButton:(UIButton*)cardButton forFaceUpStatus:(BOOL) isFaceUp;
+- (void)updateCardButton:(UIButton*)cardButton forFaceUpStatus:(BOOL) isFaceUp;
 
 // Override to set UIButton state for card unplayable status
 // Default: switches button's enabled state and changes alpha
-//- (void)updateCardButton:(UIButton*)cardButton forUnplayableStatus:(BOOL) isUnplayable;
+- (void)updateCardButton:(UIButton*)cardButton forUnplayableStatus:(BOOL) isUnplayable;
 
 // Override to convert Card's contents to NSAttributedString for displayind in UI
 // Default: contents of a card
-//- (NSAttributedString*) labelOfCard:(Card*) card;
+- (NSAttributedString*) labelOfCard:(Card*) card;
 
 // Override to return card's back image
 // Default: retuns nil
-//- (UIImage*)cardBackImage;
+- (UIImage*)cardBackImage;
 @end
